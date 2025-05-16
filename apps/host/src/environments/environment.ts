36383b537ac4af6edf03d0b1ab272e59834131b0
Trong Ngo <trong.ngotrung@gmail.com>
2025-05-16 17:10:37 +0700

    chore: set up environments for host

---
+++ apps/host/src/environments/environment.ts
@@ -0,0 +1,9 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// When building for production, this file is replaced with `environment.prod.ts`.
+
+export const environment = {
+  name: 'local',
+  production: false,
+  DOMAIN_URL: "http://localhost:3000",
+  API_BASE_URL: "http://localhost:8081"
+};
--- apps/host/src/modules/Home.tsx
+++ apps/host/src/modules/Home.tsx
@@ -1,9 +1,12 @@
+// eslint-disable-next-line @nx/enforce-module-boundaries
+import { environment } from '@host/environment';
+
 const Home = () => {
 
     return (
         <div title="host">
             <h1 className="bg-primary-100 m-0.5 truncate container">
-                Welcome Home
+                Welcome Home { environment.name }
             </h1>
 
         </div>
